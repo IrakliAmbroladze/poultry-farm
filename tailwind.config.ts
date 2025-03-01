031c3f1a3f35e77b6a4b1def7b77ee1d361c4c61
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-01 15:56:06 +0400

    refac(tailwind config): safelist
    add colors list in ts config lsafelist as without it in production dynamic color class was lost

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,14 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    "bg-green-500",
+    "hover:bg-green-600",
+    "bg-gray-500",
+    "hover:bg-gray-600",
+    "bg-blue-500",
+    "hover:bg-blue-600",
+  ],
   theme: {
     extend: {
       colors: {
